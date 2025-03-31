433fd1a53b2c7e9422721ed7f0c3a9beaf7187b1
Lysandros Nikolaou <lisandrosnik@gmail.com>
2025-03-31 10:21:29 +0200

    Fixes to env var utils

--- bot/config.py
+++ bot/config.py
@@ -6,25 +6,26 @@ from bot.exceptions import IncorrectConfigException
 
 load_dotenv()
 
-empty = object()
 
-
-def get_env_var(name: str, default: str = empty) -> str:
+def get_env_var(name: str, default: str | None = None) -> str:
     """Get an environment variable, with optional default value"""
 
     value = os.getenv(name, default)
-    if value is empty:
+    if value is None:
         raise IncorrectConfigException(f"{name} is not set")
     return value
 
 
-def get_env_var_int(name: str, default: str = empty) -> int:
+def get_env_var_int(name: str, default: int | None = None) -> int:
     """Get an environment variable as an integer, with optional default value"""
-    value = get_env_var(name, default)
+    value = os.getenv(name, default)
+    if value is None:
+        raise IncorrectConfigException(f"{name} is not set")
+
     try:
         return int(value)
     except ValueError:
-        raise IncorrectConfigException(f"{name} must be an integer")
+        raise IncorrectConfigException(f"{name} must be an integer, got {value}")
 
 
 DATABASE_URL = get_env_var("DATABASE_URL")
